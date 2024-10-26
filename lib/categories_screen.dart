8be3edfd94c78370a5c1423c47e891cd8e619049
IsmailJibril <ismail0jibril0@gmail.com>
2024-10-26 11:14:07 +0200

    3.2 nav-01-starting-setp

---
+++ lib/categories_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: [],
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+      ),
+    );
+  }
+}
